fa4aee4ebeb3e3ffb9e84eb1706c3ee12b672718
Adhikari <adhikarifeb2001@gmail.com>
2026-07-15 18:21:24 +0530

    Add cookie-parser and cors middleware for improved request handling

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,13 @@
+import { app } from './app.js';
 import { connectDB } from './db/index.js';
+import dotenv from 'dotenv/config';
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log("Server is live at port: ",process.env.PORT || 8000);
+    })
+})
+.catch(err => {
+    console.log("Server connection error: ",err);
+})
