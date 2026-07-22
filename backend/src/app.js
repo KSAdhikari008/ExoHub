910bef84670bba220f11cc1cf998a4f99e807a62
Adhikari <adhikarifeb2001@gmail.com>
2026-07-22 21:33:48 +0530

    update login page layout , styles and responsiveness

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,8 +6,8 @@ import dotenv from 'dotenv/config';
 export const app = express();
 
 // Middleware configurations.
-app.use(cors({origin: `${process.env.CORS_ORIGIN}`}));
-app.use(express.json({limit: "20kb"}));
 app.use(express.urlencoded({extended: true, limit: "20kb"}));
-app.use(express.static("public"));
+app.use(express.static("public")); // public folder available on request. 
+app.use(express.json({limit: "20kb"}));
+app.use(cors({origin: `${process.env.CORS_ORIGIN}`}));
 app.use(cookieParser());
--- frontend/src/pages/1.login/Login.jsx
+++ frontend/src/pages/1.login/Login.jsx
@@ -1,16 +1,18 @@
-import styles from "./Login.module.css";
-import heroImage from "./../../assets/loginHero.png";
 import { NavLink } from "react-router-dom";
+import heroImage from "./../../assets/loginHero.jpg";
+import styles from "./Login.module.css";
 
 function Login() {
   return (
     <div className={styles.loginPage}>
-    <div className={styles.header}>
-        <div className={styles['left-section']}>
+      <div className={styles.header}>
+        <div className={styles["left-section"]}>
           <h1>EXPOHUB</h1>
         </div>
-        <div className={styles['right-section']}>
-          <NavLink to="/register" className={styles['register']} >Register</NavLink>
+        <div className={styles["right-section"]}>
+          <NavLink to="/register" className={styles["register"]}>
+            Register
+          </NavLink>
         </div>
       </div>
       <div className={styles.main}>
@@ -21,12 +23,17 @@ function Login() {
             <p>Discover exhibitions, connect with exhibitors...</p>
           </div>
         </div>
-        <div className={styles.loginPanel}>
-          <div className={styles["login-form"]}>
-            <p>lksdfjlds</p>
-            <p>lksdfjlds</p>
-            <p>lksdfjlds</p>
-            <p>lksdfjlds</p>
+        <div className={`${styles.loginPanel} flex justify-center items-center `}>
+          <div className=" flex flex-col justify-center items-center bg-amber-200 h-9/10 w-9/10 p-7  ">
+            <div className={`${styles.title} basis-1/10`}>
+              <h3 className="text-3xl font-medium  ">Welcome Back</h3>
+              <p>Sign in to continue to your account</p>
+            </div>
+            <div className={`${styles["login-form"]} basis-9/10 bg-amber-600`}>
+              <input type="text" name="" id="" />
+              <input type="text" name="" id="" />
+              <input type="submit" value="submit" />
+            </div>
           </div>
         </div>
       </div>
