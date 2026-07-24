624c2e7d2a060dcd3ea814f3b51b08addeb69e79
Adhikari <adhikarifeb2001@gmail.com>
2026-07-24 13:23:55 +0530

    feat: set up server and database connection, refactor app initialization

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv/config';
 
-export const app = express();
+const app = express();
 
 // Middleware configurations.
 app.use(express.urlencoded({extended: true, limit: "20kb"}));
@@ -11,3 +11,5 @@ app.use(express.static("public")); // public folder available on request.
 app.use(express.json({limit: "20kb"}));
 app.use(cors({origin: `${process.env.CORS_ORIGIN}`}));
 app.use(cookieParser());
+
+export default app;
